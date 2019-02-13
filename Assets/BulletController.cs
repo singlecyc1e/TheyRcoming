e73998fb7a832b4cee9ed8302777e48ce316c470
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-02-12 17:59:07 -0800

    Added bullet script

---
+++ Assets/BulletController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BulletController : MonoBehaviour
+{
+
+    private Rigidbody2D m_rigidbody;
+    private int speed = 17;
+
+    public UnityEvent OnBulletOutOfBounds = new UnityEvent();
+    public UnityEvent OnBulletHitEnemy = new UnityEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = this.GetComponent<Rigidbody2D>();
+        //GameObject player = GameObject.FindGameObjectWithTag("Player");
+        //PlayerController controller = player.GetComponent<PlayerController>();
+        //OnBulletOutOfBounds.AddListener(controller.OnBulletOutOfBounds);
+        OnBulletOutOfBounds.AddListener(PlayerController.OnBulletOutOfBounds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rigidbody.velocity = Vector2.up * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ceiling"))
+        {
+            Debug.Log("Ceiling");
+            OnBulletOutOfBounds.Invoke();
+            Destroy(this.gameObject);
+        }
+
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            //OnBulletHitEnemy.Invoke();
+            Destroy(this.gameObject);
+        }
+    }
+}
